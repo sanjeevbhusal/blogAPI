ed731343ee123a8337624436690abedc821ca9ae
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-29 10:45:24 +0545

    added documentation for blog_api/config

--- blog_api/config.py
+++ blog_api/config.py
@@ -1,12 +1,22 @@
+"""
+Configuration for multiple stages of development like development, testing and production
+"""
+
 import os
 
 
 class DevelopmentConfiguration:
+    """
+    Development Configuration
+    """
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///dev_database.db'
 
 
 class TestingConfiguration:
+    """
+    Testing Configuration
+    """
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///test_database.db'
     TESTING = True
     SERVER_NAME = "localhost"
